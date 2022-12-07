b8908ad96c6f0b2511c3e8b76b92391b39c1cb5e
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-12-07 23:23:03 +0530

    addproduct

---
+++ src/main/java/com/nest/shoppingapp_backend/model/Shopping.java
@@ -0,0 +1,71 @@
+package com.nest.shoppingapp_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "shopping")
+public class Shopping {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+    private String category;
+    private String description;
+    private int price;
+
+    public Shopping() {
+    }
+
+    public Shopping(int id, String name, String category, String description, int price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
